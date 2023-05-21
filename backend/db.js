41331378a6b8ff9562466085585af2734a76cf71
Ram Bansal <ram.bansal@nylas.com>
2023-05-21 13:17:22 -0400

    shift queries to sqlite

--- backend/db.js
+++ backend/db.js
@@ -6,7 +6,7 @@ mkdirp.sync("./var/db");
 const db = new sqlite3.Database('./var/db/two-sided-marketplace.db')
 
   // create the database schema for the todos app
-const createTable = () => {
+const createTables = () => {
     db.run(
         "CREATE TABLE IF NOT EXISTS users ( \
             user_id TEXT PRIMARY KEY NOT NULL, \
@@ -16,10 +16,41 @@ const createTable = () => {
             salt BLOB NOT NULL \
         )"
     )
+
+    db.run(
+        "CREATE TABLE IF NOT EXISTS events ( \
+            user_id TEXT NOT NULL, \
+            calendar_id TEXT NOT NULL, \
+            event_id TEXT NOT NULL, \
+            FOREIGN KEY (user_id) REFERENCES users(user_id), \
+            FOREIGN KEY (calendar_id) REFERENCES calendars(user_id) \
+        )"
+    )
+
+    db.run(
+        "CREATE TABLE IF NOT EXISTS calendars ( \
+            user_id TEXT NOT NULL, \
+            calendar_id TEXT NOT NULL, \
+            FOREIGN KEY (user_id) REFERENCES users(user_id) \
+        )"
+    )
+    
+    // TODO: RUN THIS MIGRATION AGAIN
+
+    db.run(
+        "CREATE TABLE IF NOT EXISTS nylas_accounts ( \
+            user_id TEXT PRIMARY KEY NOT NULL, \
+            account_id TEXT NOT NULL, \
+            access_token TEXT NOT NULL, \
+            calendar_id TEXT NOT NULL, \
+            FOREIGN KEY (user_id) REFERENCES users(user_id) \
+            FOREIGN KEY (calendar_id) REFERENCES calendars(calendar_id) \
+        )"
+    )
 }
 
 db.serialize(function () {
-    createTable();
+    createTables();
 });
   
 module.exports = db;
--- backend/route.js
+++ backend/route.js
@@ -3,35 +3,37 @@ const Nylas = require('nylas');
 const crypto = require('crypto');
 const bcrypt = require("bcrypt");
 const db = require("./db");
+// TODO: Bring in dotenv
 
 const { default: Calendar } = require("nylas/lib/models/calendar");
 const StreamChat = require("stream-chat").StreamChat;
 
-const mockDb = require('./utils/mock-db');
-
 exports.readEvent = async (req, res) => {
   const user = res.locals.user;
   const eventId = req.params.id;
-  let { accessToken } = user;
+  let accessToken = null;
 
-  // TODO: Place logic as part of a middleware instead of using everywhere
+  if(user.user_type === 'patient') {
+    db.get(
+      "SELECT * FROM events where event_id = ? \
+      JOIN nylas_accounts ON events.nylas_account_id = nylas_account_id.user_id",
+      [eventId], async (err, row) => {
 
-  if(user.userType === 'patient') {
-    const { providerId } = user.events.find(event => event.id === eventId);
-    const provider = await mockDb.findUser(providerId);
-    accessToken = provider.accessToken;
+        if (err) {
+          res.status(500).json({ message: err });
+          return;
+        }
+
+        accessToken = row.access_token;
+
+        const events = await Nylas.with(accessToken).events.find(eventId);
+        return res.json(events);
+    })
   }
-
-  const events = await Nylas.with(accessToken)
-    .events.find(eventId)
-    .then((events) => events);
-
-  return res.json(events);
 };
 
 exports.readProviderEvents = async (req, res, options = {}) => {
   let events = []
-
   const user = res.locals.user;
 
   if (user.userType === 'provider') {
@@ -50,10 +52,20 @@ exports.readProviderEvents = async (req, res, options = {}) => {
       busy: false,
     }
 
-    const provider = await mockDb.findUser(req.params.id);
-    events = await Nylas.with(provider.accessToken)
-      .events.list(searchOptions)
-      .then((events) => events);
+    db.get(
+      "SELECT * FROM nylas_accounts where user_id = ?",
+      [req.params.id], async (err, row) => {
+
+        if (err) {
+          res.status(500).json({ message: err });
+          return;
+        }
+
+        accessToken = row.access_token;
+
+        events = await Nylas.with(accessToken).events.list(searchOptions)
+        return res.json(events);
+    })
   }
 
   return res.json(events);
@@ -61,38 +73,34 @@ exports.readProviderEvents = async (req, res, options = {}) => {
 
 exports.readEvents = async (req, res) => {
   const userId = req.params.userId;
-  let events = [];
+  
+  db.get(
+    "SELECT * FROM events where user_id = ? \
+    JOIN nylas_accounts ON events.nylas_account_id = nylas_account_id.user_id",
+    [userId], async (err, rows) => {
 
-  const { events: userEvents } = await mockDb.findUser(userId);
-
-  const providerDetails = await Promise.all(userEvents.map(async event => {
-    const provider = await mockDb.findUser(event.providerId);
-
-    return {
-      id: event.id,
-      accessToken: provider.accessToken,
+    if (err) {
+      res.status(500).json({ message: err });
+      return;
     }
-  }));
 
-  events = await Promise.all(providerDetails.map(async provider => {
-    const { accessToken, id } = provider;
-    const event = await Nylas.with(accessToken)
-      .events.find(id)
-      .then((events) => events);
+    const events = await Promise.all(rows.map(async data => {
+      const { access_token, event_id } = data;
+      const event = await Nylas.with(access_token)
+        .events.find(event_id)
+        .then((events) => events);
 
-    return event;
-  }));
+      return event;
+    }));
 
-  return res.json(events);
+    return res.json(events);
+  })
 };
 
-// TODO: Consider removing since we store a calendar for each virtual account
 exports.readCalendars = async (req, res) => {
   const user = res.locals.user;
 
-  const calendars = await Nylas.with(user.accessToken)
-    .calendars.list()
-    .then((calendars) => calendars);
+  const calendars = await Nylas.with(user.accessToken).calendars.list()
 
   return res.json(calendars);
 };
@@ -108,45 +116,58 @@ exports.createEvents = async (req, res, options = {}) => {
     providerId,
   } = req.body;
 
-  const provider = await mockDb.findUser(providerId);
-  const { calendarId } = provider;
+  db.get(
+    "SELECT * FROM nylas_accounts where user_id = ? \
+    JOIN calendars ON nylas_accounts.calendar_id = calendars.calendar_id",
+    [providerId], async (err, row) => {
 
-  if (!provider || !title || !startTime || !endTime) {
-    return res.status(400).json({
-      message:
-        'Missing required fields: calendarId, title, startTime or endTime',
-    });
-  }
-
-  const nylas = Nylas.with(provider.accessToken);
-  const event = new Event(nylas);
-
-  event.calendarId = calendarId;
-  event.title = title;
-  event.description = description;
-  event.when.startTime = startTime;
-  event.when.endTime = endTime;
-  // NOTE: Setting free/busy to search for availability of provider
-  event.busy = options.setAvailability ? false : true;
-
-  event.metadata = {
-    providerId: provider.id,
-    userId: user.id
-  }
-  
-  const savedEvent = await event.save();
-
-  await mockDb.updateUser(user.id, {
-    events: [
-      ...user.events, 
-      {
-        id: savedEvent.id,
-        providerId: provider.id,
+      if (err) {
+        res.status(500).json({ message: err });
+        return;
       }
-    ]
-  })
 
-  return res.json(savedEvent);
+      const { access_token, calendar_id } = row;
+
+      if (!access_token || !title || !startTime || !endTime) {
+        return res.status(400).json({
+          message:
+            'Missing required fields: providerId, title, startTime or endTime',
+        });
+      }
+
+      const nylas = Nylas.with(access_token);
+      const event = new Event(nylas);
+
+      event.calendarId = calendar_id;
+      event.title = title;
+      event.description = description;
+      event.when.startTime = startTime;
+      event.when.endTime = endTime;
+      // NOTE: Setting free/busy to search for availability of provider
+      event.busy = options.setAvailability ? false : true;
+
+      event.metadata = {
+        providerId: provider.id,
+        userId: user.id
+      }
+      
+      const savedEvent = await event.save();
+
+      const sql =
+      "INSERT INTO events (user_id, calendar_id, event_id) VALUES (?,?,?)";
+      const eventParams = [user.id, calendar_id, savedEvent.id];
+      db.run(sql, eventParams, async function (err, result) {
+
+        if (err) {
+          res.status(500).json({ message: err });
+          return;
+        }
+
+        return res.json(savedEvent);
+      });
+
+    }
+  )
 };
 
 exports.login = async (req, res) => {
@@ -166,6 +187,7 @@ exports.login = async (req, res) => {
         return;
       }
   
+      // TODO: Refactor to a utility for re-use
       const chatClient = StreamChat.getInstance(process.env.STREAM_API_KEY, process.env.STREAM_API_SECRET);
       const chatToken = chatClient.createToken(userId);
   
@@ -198,23 +220,25 @@ exports.logout = async (req, res) => {
   }
 };
 
-// TODO: Refactor logic into utilitiy functions for re-use
 exports.signup = async (req, res) => {
   try {
     const { username, password, userType } = req.body;
-    const userId =username;
+    // so userId is username
+    const userId = username;
 
     const publicId = crypto.randomUUID();
-
     const salt = bcrypt.genSaltSync(10);
     const hashed_password = bcrypt.hashSync(password, salt);
+    const cryptovector = crypto.randomBytes(32); // 16 bytes for AES-256
 
+    // TODO: multiple db.runs are nested by callbacks, refactor to use async/await
     const sql =
     "INSERT INTO users (user_id, hashed_password, public_id, user_type, salt) VALUES (?,?,?,?,?)";
     const params = [userId, hashed_password, publicId, userType, salt];
+    // TODO: 
     db.run(sql, params, async function (err, result) {
       if (err) {
-        res.status(500).json({ message: err });
+        res.status(500).json({ userInsert: err });
         return;
       }
       
@@ -224,71 +248,63 @@ exports.signup = async (req, res) => {
         clientId: process.env.NYLAS_CLIENT_ID,
       });
 
-      const { accessToken, account_id: accountId } = await Nylas.exchangeCodeForToken(code);
+      const { accessToken } = await Nylas.exchangeCodeForToken(code);
 
       console.log('Access Token was generated for: ' + username);
 
       const nylasClient = Nylas.with(accessToken);
+      const account = await nylasClient.account.get();
 
-      const calendar = new Calendar(nylasClient, {
-        name: "My New Calendar",
-        description: "Description of my new calendar",
-        location: "Location description",
-        timezone: "America/Los_Angeles",
-        metadata: {
-          test: "true",
+      // TODO: Need to encrypt this key
+      const nylasSql = 
+      "INSERT INTO nylas_accounts (user_id, account_id, access_token) VALUES (?,?,?)";
+      const nylasParams = [userId, account.id, accessToken];
+      db.run(nylasSql, nylasParams, async function (err, result) {
+
+        if (err) {
+          res.status(500).json({ nylasInsert: err });
+          return;
         }
-      })
 
-      const savedCalendar = await calendar.save()
+        const calendar = new Calendar(nylasClient, {
+          name: "My New Calendar",
+          description: "Description of my new calendar",
+          location: "Location description",
+          // TODO: Need to set timezone based on user's location
+          timezone: "America/Los_Angeles",
+          metadata: {
+            test: "test",
+          }
+        })
+  
+        const savedCalendar = await calendar.save();
 
-      const nylasAccount = await nylasClient.account.get();
+        const calendarSql = 
+        "INSERT INTO calendars (user_id, calendar_id) VALUES (?,?)";
+        const calendarParams = [userId, savedCalendar.id];
 
-      // TODO: Replace this with actual database
-      const user = await mockDb.createOrUpdateUser(publicId, {
-        accessToken,
-        emailAddress: publicId,
-        username,
-        accountId: nylasAccount.id,
-        calendarId: savedCalendar.id,
-        userType: userType || 'patient',
-        events: [],
+        db.run(calendarSql, calendarParams, async function (err, result) {
+          if (err) {
+            res.status(500).json({ calendarInsert: err });
+            return;
+          }
+
+          const chatClient = StreamChat.getInstance(process.env.STREAM_API_KEY, process.env.STREAM_API_SECRET);
+          const chatToken = chatClient.createToken(userId);
+  
+          chatClient.upsertUser({
+            id: userId,
+            username: username,
+          });
+  
+          return res.status(200).json({
+            chatToken: chatToken,
+            userId: userId,
+            publicId: publicId,
+            userType: userType,
+          })
+        });
       });
-
-      const chatClient = StreamChat.getInstance(process.env.STREAM_API_KEY, process.env.STREAM_API_SECRET);
-      const chatToken = chatClient.createToken(userId);
-
-      chatClient.upsertUser({
-        id: userId,
-        username: username,
-      });
-
-      return res.status(200).json({
-        chatToken: chatToken,
-        userId: userId,
-        publicId: publicId,
-        userType: user.userType,
-      })
-    });
-
-    const chatClient = StreamChat.getInstance(process.env.STREAM_API_KEY, process.env.STREAM_API_SECRET);
-
-    const chatToken = chatClient.createToken(userId);
-
-    chatClient.upsertUser({
-      id: userId,
-      username: username,
-    });
-
-    return res.status(200).json({
-      chatToken: chatToken,
-      userId: row.user_id,
-      publicId: row.public_id,
-      userType: row.user_type,
-    })
-
-    // TODO: Collect tokens and return these too in the response
-    return res.status(200).json(row);
     });
   } catch (err) {
     console.log(err);
@@ -296,15 +312,9 @@ exports.signup = async (req, res) => {
   }
 }
 
+// TODO: Refactor this endpoint, what is the purpose of this?
 exports.readUser = async (req, res) => {
-  // TODO: Limit to the user to request this information
-  const userId = req.params.userId;
   const user = res.locals.user;
-
-  // console.log(userId);
-  // if (user.id !== userId) {
-    // return res.status(401).json('Unauthorized');
-  // }
 
   Nylas.config({
     clientId: process.env.NYLAS_CLIENT_ID, 
@@ -314,23 +324,27 @@ exports.readUser = async (req, res) => {
 
   const account = await Nylas.accounts.find(user.accountId).then((user) => user);
 
-  // TODO: Check what is returned, see if you need to augment with datastore
   return res.json(account);
 }
 
 exports.readProviders = async (req, res) => {
-  const user = res.locals.user;
-  
-  const data = await mockDb.findProviders();
+  db.get(
+    "SELECT * FROM users where user_type = ?",
+    ["provider"], async (err, rows) => {
 
-  // TODO: Consider storing and returning additional provider details
-  const providers = data.map(provider => ({
-    id: provider.id,
-    username: provider.username,
-    emailAddress: provider.emailAddress
-  }))
+      if (err) {
+        res.status(500).json({ message: err });
+        return;
+      }
 
-  return res.json(providers);
+      // TODO: Consider storing and returning additional provider details
+      const providers = rows.map(provider => ({
+        id: provider.user_id,
+        username: provider.user_id
+      }))
+
+      return res.json(providers);
+  })
 }
 
 // TODO: Restrict to admin
@@ -342,33 +356,51 @@ exports.deleteUser = (req, res) => {
     clientSecret: process.env.NYLAS_CLIENT_SECRET,
   });
 
-  const result = Nylas.accounts.delete('{id}').then(result => result);
+  const result = Nylas.accounts.delete(`${userId}`).then(result => result);
 
-  return res.json(result);
+  db.run("DELETE FROM users WHERE user_id = ?", [userId], function (err) {
+    if (err) {
+      res.status(500).json({ message: err });
+      return;
+    }
+
+    return res.json(result);
+  });
 }
 
 exports.deleteEvent = async (req, res) => {
   const user = res.locals.user;
   const eventId = req.params.eventId;
-  let accessToken = user.accessToken
+  let accessToken = user.access_token
 
-  if(user.userType === 'patient') {
-    const { providerId } = user.events.find(event => event.id === eventId);
-    const provider = await mockDb.findUser(providerId);
-    accessToken = provider.accessToken;
+  if(user.user_type === 'patient') {
+    db.get(
+      "SELECT * FROM events where event_id = ? \
+      JOIN nylas_accounts on users.user_id = nylas_accounts.user_id",
+      [eventId], async (err, rows) => {
+
+        if (err) {
+          res.status(500).json({ message: err });
+          return;
+        }
+
+        accessToken = rows.access_token;
+      }
+    )
+
+    let nylas = Nylas.with(user.accessToken);
+    
+    const result = await nylas.events.delete([eventId]);
+
+    db.run("DELETE FROM events WHERE event_id = ?", [eventId], function (err) {
+      if (err) {
+        res.status(500).json({ message: err });
+        return;
+      }
+  
+      return res.json(result);
+    });
   }
-
-  let nylas = Nylas.with(user.accessToken);
-
-  const result = await nylas.events.delete([eventId]).then(result => result);
-
-  const updatedEvents = user.events.filter(event => event.id !== eventId);
-
-  await mockDb.updateUser(user.id, {
-    events: updatedEvents
-  })
-
-  return res.json(result);
 }
 
 exports.updateEvent = async (req, res) => {
@@ -385,25 +417,33 @@ exports.updateEvent = async (req, res) => {
 
   let accessToken = user.accessToken
 
-  if(user.userType === 'patient') {
-    const { providerId } = user.events.find(event => event.id === eventId);
-    const provider = await mockDb.findUser(providerId);
-    accessToken = provider.accessToken;
+  if(user.user_type === 'patient') {
+    db.get(
+      "SELECT * FROM events where event_id = ? \
+      JOIN nylas_accounts ON events.nylas_account_id = nylas_account_id.user_id",
+      [id], async (err, row) => {
+
+        if (err) {
+          res.status(500).json({ message: err });
+          return;
+        }
+
+        accessToken = row.access_token;
+        let nylas = Nylas.with(user.accessToken);
+
+        const event = nylas.events.find(id).then((event) => event);
+      
+        const updatedEvent = {
+          ...event,
+          title,
+          description,
+          startTime,
+          endTime,
+        }
+      
+        const result = nylas.events.save([updatedEvent]).then(result => result);
+      
+        return res.json(result);
+    })
   }
-
-  let nylas = Nylas.with(user.accessToken);
-
-  const event = nylas.events.find(id).then((event) => event);
-
-  const updatedEvent = {
-    ...event,
-    title,
-    description,
-    startTime,
-    endTime,
-  }
-
-  const result = nylas.events.save([updatedEvent]).then(result => result);
-
-  return res.json(result);
 }
